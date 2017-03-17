485ecef8d9255833cc6707ff0b7d388db046b91b
Joonas Javanainen <joonas.javanainen@gmail.com>
2017-03-18 01:17:23 +0200

    Add some AGB photos

--- src/site/pages/Agb.tsx
+++ src/site/pages/Agb.tsx
@@ -30,11 +30,21 @@ export default function Agb({submissions}: Props) {
   )
 }
 
-function Submission({submission: {contributor, slug, title, metadata}}: {submission: AgbSubmission}) {
+function Submission({submission: {contributor, slug, title, metadata, photos}}: {submission: AgbSubmission}) {
   return (
     <tr>
       <td className="submission-list-item">
         <a className="submission-list-item__link">
+          <div className="submission-list-item__photo">
+            {photos.front
+              ? <img
+                src={`/static/agb/${slug}_thumbnail_80.jpg`}
+                srcSet={`/static/agb/${slug}_thumbnail_50.jpg 50w, /static/agb/${slug}_thumbnail_80.jpg 80w`}
+                sizes="(min-width: 1000px) 80px, 50px"
+                role="presentation" />
+              : null
+            }
+          </div>
           <div className="submission-list-item__id">
             <div className="submission-list-item__title">{title}</div>
             <aside className="submission-list-item__contributor">{contributor}</aside>

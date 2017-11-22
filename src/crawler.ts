c3650d6d68a28ef8a8d4bee23d54aeb092c693e5
Joonas Javanainen <joonas.javanainen@gmail.com>
2017-11-22 22:28:41 +0200

    Tidy up code

--- src/crawler.ts
+++ src/crawler.ts
@@ -38,243 +38,145 @@ export interface Photo {
   stats: fs.Stats;
 }
 
-export interface DmgSubmission {
-  type: 'dmg';
+interface SubmissionBase<T extends string, M extends Metadata, P = DefaultPhotos> {
+  type: T;
   title: string;
   slug: string;
   contributor: string;
-  metadata: DmgMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    mainboardFront?: Photo;
-    mainboardBack?: Photo;
-    lcdBoardFront?: Photo;
-    lcdBoardBack?: Photo;
-    powerBoardFront?: Photo,
-    powerBoardBack?: Photo;
-    jackBoardFront?: Photo;
-    jackBoardBack?: Photo;
+  metadata: M;
+  photos: P;
+}
+
+export interface DefaultPhotos {
+  front?: Photo;
+  back?: Photo;
+  pcbFront?: Photo;
+  pcbBack?: Photo;
+}
+
+export interface DmgPhotos {
+  front?: Photo;
+  back?: Photo;
+  mainboardFront?: Photo;
+  mainboardBack?: Photo;
+  lcdBoardFront?: Photo;
+  lcdBoardBack?: Photo;
+  powerBoardFront?: Photo,
+  powerBoardBack?: Photo;
+  jackBoardFront?: Photo;
+  jackBoardBack?: Photo;
+}
+
+export type DmgSubmission = SubmissionBase<'dmg', DmgMetadata, DmgPhotos>;
+export type SgbSubmission = SubmissionBase<'sgb', SgbMetadata>;
+export type MgbSubmission = SubmissionBase<'mgb', MgbMetadata>;
+export type MglSubmission = SubmissionBase<'mgl', MglMetadata>;
+export type Sgb2Submission = SubmissionBase<'sgb2', Sgb2Metadata>;
+export type CgbSubmission = SubmissionBase<'cgb', CgbMetadata>;
+export type AgbSubmission = SubmissionBase<'agb', AgbMetadata>;
+export type AgsSubmission = SubmissionBase<'ags', AgsMetadata>;
+export type GbsSubmission = SubmissionBase<'gbs', GbsMetadata>;
+export type OxySubmission = SubmissionBase<'oxy', OxyMetadata>;
+
+async function crawlDefaultPhotos(unit: FsEntry): Promise<DefaultPhotos> {
+  const [front, back, pcbFront, pcbBack] = await Promise.all([
+    '01_front.jpg',
+    '02_back.jpg',
+    '03_pcb_front.jpg',
+    '04_pcb_back.jpg',
+  ].map(filename => fetchPhoto(unit, filename)));
+  return {front, back, pcbFront, pcbBack};
+}
+
+async function crawlDmgPhotos(unit: FsEntry): Promise<DmgPhotos> {
+  const [
+    front, back, mainboardFront, mainboardBack, lcdBoardFront, lcdBoardBack,
+    powerBoardFront, powerBoardBack, jackBoardFront, jackBoardBack
+  ] = await Promise.all([
+    '01_front.jpg',
+    '02_back.jpg',
+    '03_mainboard_front.jpg',
+    '04_mainboard_back.jpg',
+    '05_lcd_board_front.jpg',
+    '06_lcd_board_back.jpg',
+    '07_power_board_front.jpg',
+    '08_power_board_back.jpg',
+    '09_jack_board_front.jpg',
+    '10_jack_board_back.jpg',
+  ].map(filename => fetchPhoto(unit, filename)));
+  return {
+    front, back, mainboardFront, mainboardBack, lcdBoardFront, lcdBoardBack,
+    powerBoardFront, powerBoardBack, jackBoardFront, jackBoardBack
   };
 }
 
-export interface SgbSubmission {
-  type: 'sgb';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: SgbMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
+interface SubmissionPath {
+  contributor: FsEntry;
+  model: FsEntry;
+  unit: FsEntry;
 }
 
-export interface MgbSubmission {
-  type: 'mgb';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: MgbMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
+function parseUnitName({contributor, unit}: SubmissionPath) {
+  if (/^[A-Z]+[0-9]+(-[0-9])?$/.test(unit.name)) {
+    return {title: unit.name, slug: unit.name};
+  } else if (/^[0-9]+(-[0-9])?$/.test(unit.name)) {
+    return {title: `Unit #${unit.name}`, slug: urlSlug(`${contributor}-${unit.name}`)};
+  } else {
+    throw new Error(`Unsupported unit name format "${unit.name}"`);
+  }
 }
 
-export interface MglSubmission {
-  type: 'mgl';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: MglMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
-}
-
-export interface Sgb2Submission {
-  type: 'sgb2';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: Sgb2Metadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
-}
-
-export interface CgbSubmission {
-  type: 'cgb';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: CgbMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
-}
-
-export interface AgbSubmission {
-  type: 'agb';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: AgbMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
-}
-
-export interface AgsSubmission {
-  type: 'ags';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: AgsMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
-}
-
-export interface GbsSubmission {
-  type: 'gbs';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: GbsMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
-}
-
-export interface OxySubmission {
-  type: 'oxy';
-  title: string;
-  slug: string;
-  contributor: string;
-  metadata: OxyMetadata;
-  photos: {
-    front?: Photo;
-    back?: Photo;
-    pcbFront?: Photo;
-    pcbBack?: Photo;
-  };
+async function crawl<T extends string, M extends Metadata, P = DefaultPhotos>(
+  type: T,
+  schema: Joi.Schema,
+  photoCrawler: (unit: FsEntry) => Promise<P>,
+  path: SubmissionPath
+): Promise<SubmissionBase<T, M, P> | undefined> {
+  const {contributor, unit} = path;
+  const metadata = await readMetadata<M>(unit, schema);
+  if (!metadata) return undefined;
+  const {title, slug} = parseUnitName(path)
+  const photos = await photoCrawler(unit)
+  return {type, title, slug, contributor: contributor.name, metadata, photos}
 }
 
 export async function crawlDataDirectory(path: string): Promise<Submission[]> {
-  interface Submission {
-    contributor: FsEntry;
-    model: FsEntry;
-    unit: FsEntry;
-  }
   const contributors = await directories(path);
-  const submissions = R.flatten<Submission>(await Promise.all(contributors.map(async contributor => {
+  const submissions = R.flatten<SubmissionPath>(await Promise.all(contributors.map(async contributor => {
     const models = await directories(contributor.absolutePath);
-    return R.flatten<Submission>(await Promise.all(models.map(async model => {
+    return R.flatten<SubmissionPath>(await Promise.all(models.map(async model => {
       const units = await directories(model.absolutePath);
       return units.map(unit => ({contributor, model, unit}))
     })));
   })));
-  return rejectNil(await Promise.all(submissions.map(async ({contributor, model, unit}) => {
-    switch(model.name) {
-      case 'DMG': {
-        const metadata = await readMetadata<DmgMetadata>(unit, DmgMetadata.schema);
-        if (metadata) {
-          return crawlDMG(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'SGB': {
-        const metadata = await readMetadata<SgbMetadata>(unit, SgbMetadata.schema);
-        if (metadata) {
-          return crawlSGB(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'MGB': {
-        const metadata = await readMetadata<MgbMetadata>(unit, MgbMetadata.schema);
-        if (metadata) {
-          return crawlMGB(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'MGL': {
-        const metadata = await readMetadata<MglMetadata>(unit, MglMetadata.schema);
-        if (metadata) {
-          return crawlMGL(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'SGB2': {
-        const metadata = await readMetadata<Sgb2Metadata>(unit, Sgb2Metadata.schema);
-        if (metadata) {
-          return crawlSGB2(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'CGB': {
-        const metadata = await readMetadata<CgbMetadata>(unit, CgbMetadata.schema);
-        if (metadata) {
-          return crawlCGB(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'AGB': {
-        const metadata = await readMetadata<AgbMetadata>(unit, AgbMetadata.schema);
-        if (metadata) {
-          return crawlAGB(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'AGS': {
-        const metadata = await readMetadata<AgsMetadata>(unit, AgsMetadata.schema);
-        if (metadata) {
-          return crawlAGS(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'GBS': {
-        const metadata = await readMetadata<GbsMetadata>(unit, GbsMetadata.schema);
-        if (metadata) {
-          return crawlGBS(contributor.name, unit, metadata);
-        }
-        break;
-      }
-      case 'OXY': {
-        const metadata = await readMetadata<OxyMetadata>(unit, OxyMetadata.schema);
-        if (metadata) {
-          return crawlOXY(contributor.name, unit, metadata);
-        }
-        break;
-      }
+  return rejectNil(await Promise.all(submissions.map(async path => {
+    const {model} = path
+    switch (model.name) {
+      case 'DMG':
+        return await crawl<'dmg', DmgMetadata, DmgPhotos>('dmg', DmgMetadata.schema, crawlDmgPhotos, path);
+      case 'SGB':
+        return await crawl<'sgb', SgbMetadata>('sgb', SgbMetadata.schema, crawlDefaultPhotos, path);
+      case 'MGB':
+        return await crawl<'mgb', MgbMetadata>('mgb', MgbMetadata.schema, crawlDefaultPhotos, path);
+      case 'MGL':
+        return await crawl<'mgl', MglMetadata>('mgl', MglMetadata.schema, crawlDefaultPhotos, path);
+      case 'SGB2':
+        return await crawl<'sgb2', Sgb2Metadata>('sgb2', Sgb2Metadata.schema, crawlDefaultPhotos, path);
+      case 'CGB':
+        return await crawl<'cgb', CgbMetadata>('cgb', CgbMetadata.schema, crawlDefaultPhotos, path);
+      case 'AGB':
+        return await crawl<'agb', AgbMetadata>('agb', AgbMetadata.schema, crawlDefaultPhotos, path);
+      case 'AGS':
+        return await crawl<'ags', AgsMetadata>('ags', AgsMetadata.schema, crawlDefaultPhotos, path);
+      case 'GBS':
+        return await crawl<'gbs', GbsMetadata>('gbs', GbsMetadata.schema, crawlDefaultPhotos, path);
+      case 'OXY':
+        return await crawl<'oxy', OxyMetadata>('oxy', OxyMetadata.schema, crawlDefaultPhotos, path);
       default: {
         console.warn(`Skipping unknown model directory ${model.absolutePath}`);
+        return undefined
       }
     }
-    return undefined
   })));
 }
 
@@ -287,142 +189,6 @@ async function readMetadata<T extends Metadata>(unit: FsEntry, schema: Joi.Schem
     return undefined;
   }
   return validationResult.value;
-}
-
-async function crawlDMG(contributor: string, unit: FsEntry, metadata: DmgMetadata): Promise<DmgSubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    mainboardFront: await fetchPhoto(unit, '03_mainboard_front.jpg'),
-    mainboardBack: await fetchPhoto(unit, '04_mainboard_back.jpg'),
-    lcdBoardFront: await fetchPhoto(unit, '05_lcd_board_front.jpg'),
-    lcdBoardBack: await fetchPhoto(unit, '06_lcd_board_back.jpg'),
-    powerBoardFront: await fetchPhoto(unit, '07_power_board_front.jpg'),
-    powerBoardBack: await fetchPhoto(unit, '08_power_board_back.jpg'),
-    jackBoardFront: await fetchPhoto(unit, '09_jack_board_front.jpg'),
-    jackBoardBack: await fetchPhoto(unit, '10_jack_board_back.jpg'),
-  };
-
-  return {type: 'dmg', title, slug, contributor, metadata, photos};
-}
-
-async function crawlSGB(contributor: string, unit: FsEntry, metadata: SgbMetadata): Promise<SgbSubmission> {
-  const title = `Unit #${unit.name}`;
-  const slug = urlSlug(`${contributor}-${unit.name}`);
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'sgb', title, slug, contributor, metadata, photos};
-}
-
-async function crawlMGB(contributor: string, unit: FsEntry, metadata: MgbMetadata): Promise<MgbSubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'mgb', title, slug, contributor, metadata, photos};
-}
-
-async function crawlMGL(contributor: string, unit: FsEntry, metadata: MglMetadata): Promise<MglSubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'mgl', title, slug, contributor, metadata, photos};
-}
-
-async function crawlSGB2(contributor: string, unit: FsEntry, metadata: Sgb2Metadata): Promise<Sgb2Submission> {
-  const title = `Unit #${unit.name}`;
-  const slug = urlSlug(`${contributor}-${unit.name}`);
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'sgb2', title, slug, contributor, metadata, photos};
-}
-
-async function crawlCGB(contributor: string, unit: FsEntry, metadata: CgbMetadata): Promise<CgbSubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'cgb', title, slug, contributor, metadata, photos};
-}
-
-async function crawlAGB(contributor: string, unit: FsEntry, metadata: AgbMetadata): Promise<AgbSubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'agb', title, slug, contributor, metadata, photos};
-}
-
-async function crawlAGS(contributor: string, unit: FsEntry, metadata: AgsMetadata): Promise<AgsSubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'ags', title, slug, contributor, metadata, photos};
-}
-
-async function crawlGBS(contributor: string, unit: FsEntry, metadata: GbsMetadata): Promise<GbsSubmission> {
-  const title = `Unit #${unit.name}`;
-  const slug = urlSlug(`${contributor}-${unit.name}`);
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'gbs', title, slug, contributor, metadata, photos};
-}
-
-async function crawlOXY(contributor: string, unit: FsEntry, metadata: OxyMetadata): Promise<OxySubmission> {
-  const title = unit.name;
-  const slug = unit.name;
-  const photos = {
-    front: await fetchPhoto(unit, '01_front.jpg'),
-    back: await fetchPhoto(unit, '02_back.jpg'),
-    pcbFront: await fetchPhoto(unit, '03_pcb_front.jpg'),
-    pcbBack: await fetchPhoto(unit, '04_pcb_back.jpg'),
-  };
-
-  return {type: 'oxy', title, slug, contributor, metadata, photos};
 }
 
 async function fetchPhoto(entry: FsEntry, name: string): Promise<Photo | undefined> {

0953872c944a09d65a275c1d9e08a4ba30cd9a32
Joonas Javanainen <joonas.javanainen@gmail.com>
2022-05-08 17:43:08 +0300

    Upgrade Rust dependencies

--- tools/src/bin/gbhwdb-photo.rs
+++ tools/src/bin/gbhwdb-photo.rs
@@ -1,7 +1,11 @@
 use anyhow::Error;
 use clap::{value_t, App, Arg, ArgMatches};
 use image::{imageops::FilterType, ImageOutputFormat};
-use std::{fs::File, io, u32};
+use std::{
+    fs::File,
+    io::{self, Cursor, Write},
+    u32,
+};
 
 fn run(matches: &ArgMatches) -> Result<(), Error> {
     let width = value_t!(matches, "width", u32).unwrap_or(u32::MAX);
@@ -11,8 +15,10 @@ fn run(matches: &ArgMatches) -> Result<(), Error> {
     let img = image::open(&input)?.resize(width, height, FilterType::Lanczos3);
     let format = ImageOutputFormat::Jpeg(80);
     if output == "-" {
+        let mut buffer = Cursor::new(Vec::new());
+        img.write_to(&mut buffer, format)?;
         let mut w = io::stdout();
-        img.write_to(&mut w, format)?;
+        w.write_all(buffer.get_ref())?;
     } else {
         let mut w = File::create(output)?;
         img.write_to(&mut w, format)?;
@@ -24,7 +30,7 @@ fn main() -> Result<(), Error> {
     let matches = App::new("gbhwdb-photo")
         .arg(
             Arg::with_name("output")
-                .short("o")
+                .short('o')
                 .long("output")
                 .value_name("OUTPUT")
                 .required(true)
@@ -38,14 +44,14 @@ fn main() -> Result<(), Error> {
         )
         .arg(
             Arg::with_name("width")
-                .short("w")
+                .short('w')
                 .long("width")
                 .value_name("WIDTH")
                 .required_unless("height"),
         )
         .arg(
             Arg::with_name("height")
-                .short("h")
+                .short('h')
                 .long("height")
                 .value_name("HEIGHT")
                 .required_unless("width"),
